cd073fc16baeef4e222604c96410c7204c823620
Paul Meffle <meffle.paul@me.com>
2016-06-09 10:25:06 +0200

    Add body of Obstacle class and fix some formatting

--- src/App.h
+++ src/App.h
@@ -6,53 +6,53 @@
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <vector>
 
-#define EPSILON				0.0000001f
-#define BOID_SPEED			5.f
-#define MAX_SPEED			70.f
-#define MOUSE_ATTRACTION	0.01f
-#define MOUSE_REPULSION		1.f
-#define REPULSION_DIST		250.f
+#define EPSILON             0.0000001f
+#define BOID_SPEED          5.f
+#define MAX_SPEED           70.f
+#define MOUSE_ATTRACTION    0.01f
+#define MOUSE_REPULSION     1.f
+#define REPULSION_DIST      150.f
 #define REPULSION_DIST_FACTOR 50.f
-#define CENTER_WEIGHT		100.f
-#define INFLUENCE_DIST		50.f
-#define INFLUENCE_FACTOR	1.5f
-#define BOID_ATTRACTION		50.f
+#define CENTER_WEIGHT       100.f
+#define INFLUENCE_DIST      50.f
+#define INFLUENCE_FACTOR    1.5f
+#define BOID_ATTRACTION     50.f
 
 namespace boids
 {
 class App
 {
 private:
-	uint16 width;
-	uint16 height;
-	float worldwidth;
-	float worldheight;
-	sf::RenderWindow window;
-	std::vector<Boid*> boidslist;
+    uint16 width;
+    uint16 height;
+    float worldwidth;
+    float worldheight;
+    sf::RenderWindow window;
+    std::vector<Boid*> boidslist;
 
-	bool followMouse;
-	bool fleeMouse;
+    bool followMouse;
+    bool fleeMouse;
 
-	void addBoid();
-	void addBoid(const sf::Vector2f& position);
-	void addBoid(const sf::Vector2f& position, const sf::Vector2f& velocity);
+    void addBoid();
+    void addBoid(const sf::Vector2f& position);
+    void addBoid(const sf::Vector2f& position, const sf::Vector2f& velocity);
 
-	sf::Vector2f rule1(Boid* b);
-	sf::Vector2f rule2(Boid* b);
-	sf::Vector2f rule3(Boid* b);
-	sf::Vector2f rule4(Boid* b);
+    sf::Vector2f rule1(Boid* b);
+    sf::Vector2f rule2(Boid* b);
+    sf::Vector2f rule3(Boid* b);
+    sf::Vector2f rule4(Boid* b);
 
-	void limitVelocities();
+    void limitVelocities();
 
-	void handleEvent(sf::Event& ev);
-	void update(float dt);
-	void draw();
+    void handleEvent(sf::Event& ev);
+    void update(float dt);
+    void draw();
 
 public:
-	App(uint16 width, uint16 height);
-	~App();
+    App(uint16 width, uint16 height);
+    ~App();
 
-	int run();
+    int run();
 };
 }
 
--- src/Obstacle.h
+++ src/Obstacle.h
@@ -1,11 +1,21 @@
 #ifndef OBSTACLE_H
 #define OBSTACLE_H
 
+#include "Types.h"
+#include <SFML/System/Vector2.hpp>
+#include <SFML/Graphics/Color.hpp>
+
 namespace boids
 {
 class Obstacle
 {
 public:
+	sf::Vector2f position;
+	sf::Color color;
+	float size;
+
+	Obstacle(const sf::Vector2f& position, float size = 50);
+	~Obstacle();
 
 };
 }
